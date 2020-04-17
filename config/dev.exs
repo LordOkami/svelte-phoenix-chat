81f21d9b8da84e6f95c8d4e5aa0a450631582149
Luis Sebastian Huerta <luissebastianhuerta@gmail.com>
2020-04-17 10:04:41 +0200

    chore: deploy front

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,7 @@ config :gat_chat, GatChatWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [yarn: ["deploy", cd: Path.expand("../assets", __DIR__)]]
 
 # Configure your database
 config :gat_chat, GatChat.Repo,
